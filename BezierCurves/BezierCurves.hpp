1fcf9c94149d1900c37723db89a826e9ef5ad9d9
psde <me@mathias-garbe.de>
2010-02-08 01:45:26 +0100

    quick cleanup

--- BezierCurves/BezierCurves.hpp
+++ BezierCurves/BezierCurves.hpp
@@ -57,10 +57,8 @@ namespace Gosu
 			glUniform1f(glGetUniformLocation(program.program, "width"), w/2);
 			glUniform1f(glGetUniformLocation(program.program, "step"), 1.0 / (float) segments);
 
-
 			glUniform4f(glGetUniformLocation(program.program, "color"), r/255, g/255, b/255, a/255);
-
-			//glColor4f (r, g, b, a);
+			
 			glBegin(GL_QUAD_STRIP);
 			
 			glVertex3f(SEGMENT_START1, -1.0f, 0.0f);
@@ -68,21 +66,13 @@ namespace Gosu
 			
 			for(int i = 0; i <= segments; i++)
 			{
-				//glUniform1f(glGetUniformLocation(program.program, "lvl"), i / (float) segments);
 				glVertex3f(SEGMENT_MID1, (float)i, (float)i / (float) segments);
 				glVertex3f(SEGMENT_MID2, (float)i, (float)i / (float) segments);
 			}
 
-			//glUniform1f(glGetUniformLocation(program.program, "lvl"), 1.0f);
-
 			glVertex3f(SEGMENT_END1, -1.0f, 1.0f);
 			glVertex3f(SEGMENT_END2, -1.0f, 1.0f);
 
-
-			glVertex3f(100, 101.0f, 1.0f);
-			glVertex3f(101, 100.0f, 1.0f);
-			glVertex3f(200, 100.0f, 1.0f);
-			glVertex3f(200, 101.0f, 1.0f);
 
 			glEnd();
 	
